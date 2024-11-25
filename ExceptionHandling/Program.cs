411e61b49b5afe0074c61f7b9577048a0ab9cd7a
Anas Delair <24ADelair@heckgrammar.co.uk>
2024-11-25 09:56:22 +0000

    Add project files.

---
+++ ExceptionHandling/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Text.RegularExpressions;
+
+namespace ExceptionHandling
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("Enter a number: ");
+                    int input = Convert.ToInt32(Console.ReadLine());
+                    break;
+                }
+                catch
+                {
+                    Console.WriteLine("Not a number");
+                }
+            }
+        }
+    }
+}
